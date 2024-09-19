2b2841a5d51fdc01c492c5fe44a4127207f098b1
kevserozkan <sumeyye.kevser.ozkan@finartz.com>
2024-09-19 17:11:19 +0300

    add metadata for opengraph to list page

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -2,21 +2,26 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { fetchRecipes } from '@/app/actions'
 import { Recipe } from '@/app/types'
-import Head from 'next/head'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Recipes',
+  description: 'Find home made recipes!',
+  category: 'food',
+  keywords: 'recipe, recipes, homemade, delicious',
+  openGraph: {
+    url: 'https://seo-app-woad.vercel.app/recipes',
+    type: 'website',
+    title: 'Recipe list',
+    description: 'Homemade recipes that you can make at home.',
+  },
+}
 
 export default async function Home() {
   const data: Recipe[] = await fetchRecipes()
 
   return (
     <div>
-      <Head>
-        <title>Recipes</title>
-        <meta name={'description'} content={'Find home made recipes!'} />
-        <meta
-          name={'keywords'}
-          content={'recipe, recipes, homemade, delicious'}
-        />
-      </Head>
       <ul>
         {data.map((item: Recipe) => (
           <li key={item.id} className={'card'}>
