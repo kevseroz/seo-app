b561c612994e4dc95c37b9f052cc66506740c5b4
kevserozkan <sumeyye.kevser.ozkan@finartz.com>
2024-09-20 10:20:48 +0300

    add metadata for detail page

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -2,18 +2,43 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { PageProps, Recipe } from '@/app/types'
 import { fetchRecipe } from '@/app/actions'
-import Head from 'next/head'
+import type { Metadata, ResolvingMetadata } from 'next'
 
+type Props = {
+  params: { id: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.id
+
+  const data: Recipe = await fetchRecipe(id)
+
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: data.title,
+    description: data.title,
+    keywords: data.title,
+    openGraph: {
+      url: `https://seo-app-woad.vercel.app/recipes/${id}`,
+      title: data.title,
+      description: data.title,
+      images: [
+        `https://seo-app-woad.vercel.app/images/${data.title.toLowerCase()}.jpg`,
+        ...previousImages,
+      ],
+      type: 'website',
+    },
+  }
+}
 export default async function Page({ params }: PageProps) {
   const data: Recipe = await fetchRecipe(params.id)
 
   return (
     <>
-      <Head>
-        <title>{data.title}</title>
-        <meta name={'description'} content={data.title} />
-        <meta name={'keywords'} content={data.title} />
-      </Head>
       <div className={'button-container'}>
         <Link href={'/recipes'}>
           <button className={'back-button'}>Back To Recipes</button>
